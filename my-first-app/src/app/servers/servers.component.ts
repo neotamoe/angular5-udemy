773a03786b83983958769e638d9e34cb1935db71
neotamoe <neota.moe@gmail.com>
2018-04-16 19:46:06 -0500

    different ways to do a selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers',  // html would be <app-servers></app-servers>
+  // selector: '[app-servers]'  // html would need  app-servers as a custom attribute
+  // selector: '.app-servers'  // html would need class of app-servers
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
