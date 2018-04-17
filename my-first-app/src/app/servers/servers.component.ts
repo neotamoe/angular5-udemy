98c2a29cd63f6b621d597bedfbb20797df34c7e6
neotamoe <neota.moe@gmail.com>
2018-04-17 11:02:58 -0500

    completed lessons on data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,14 +4,28 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',  // html would be <app-servers></app-servers>
   // selector: '[app-servers]'  // html would need  app-servers as a custom attribute
   // selector: '.app-servers'  // html would need class of app-servers
+  // template: `  // template can be used instead of templateUrl
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => { this.allowNewServer = true; }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
