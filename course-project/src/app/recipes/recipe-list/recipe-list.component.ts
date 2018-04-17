57b540215bdbf231f64e6d42b28ce319c26b31f5
neotamoe <neota.moe@gmail.com>
2018-04-17 15:36:33 -0500

    recipe components basics started

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'this is a test, I repeat...this is a test',
+      'http://bradleysbarandgrill.com/wp-content/uploads/2017/05/bar-food.jpg'),
+  ];
 
   constructor() { }
 
